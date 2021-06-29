e834789e518c7421e61f8ee55de257040c528fe9
Cat Crusader <79837862+cat-crusader@users.noreply.github.com>
2021-06-29 14:59:12 +0000

    everyting

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const Discord = require("discord.js")
+const { Client, MessageEmbed } = require('discord.js');
 const client = new Discord.Client()
+
+
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
+
 })
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
-})
+// client.on("message", msg => {
+//   if (msg.content === "міу") {
+//     msg.channel.send('лох');
+//   }
+// //   if (msg.content.startsWith('!update')) {
+// //     // let indOfDot = msg.content.indexOf('.', 0);
+// //     // let title;
+// //     // let content;
+// //     // if(indOfDot<=7)
+// //     // title = msg.content.slice(7);
+// //     // else
+// //     // title = msg.content.slice(7,indOfDot);
+// // // while(indOfDot!=-1)
+// // // {
+// // // indOfDot = msg.content.indexOf('.', indOfDot);
 
+// // // }
+
+
+// //         const embed = new MessageEmbed()
+// //       // Set the title of the field
+// //       .setTitle('l')
+// //       // Set the color of the embed
+// //       .setColor(0xff0000)
+// //       // Set the main content of the embed
+// //       .setDescription('Hello, this is a \n slick embed!');
+
+// //   // client.channels.fetch('784545317432983573').send('hi');
+
+// //   }
+  
+// })
+
+
+//
 client.login(process.env.TOKEN)
