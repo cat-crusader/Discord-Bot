093e837f332dd0b8a4ec04cf99b535fa2b9de053
Cat Crusader <79837862+cat-crusader@users.noreply.github.com>
2021-06-29 16:03:09 +0000

    Add update function(write log in discord -> bot post it in embded format with some features)

--- index.js
+++ index.js
@@ -9,38 +9,48 @@ client.on("ready", () => {
 
 })
 
-// client.on("message", msg => {
-//   if (msg.content === "міу") {
-//     msg.channel.send('лох');
-//   }
-// //   if (msg.content.startsWith('!update')) {
-// //     // let indOfDot = msg.content.indexOf('.', 0);
-// //     // let title;
-// //     // let content;
-// //     // if(indOfDot<=7)
-// //     // title = msg.content.slice(7);
-// //     // else
-// //     // title = msg.content.slice(7,indOfDot);
-// // // while(indOfDot!=-1)
-// // // {
-// // // indOfDot = msg.content.indexOf('.', indOfDot);
-
-// // // }
+client.on("message", msg => {
+  if (msg.content === "міу") {
+    msg.channel.send('лох');
+  }
+  if (msg.content.startsWith('!update')) {
+    let indOfDot = msg.content.indexOf('.', 0);
+    let title;
+    let content="";
 
 
-// //         const embed = new MessageEmbed()
-// //       // Set the title of the field
-// //       .setTitle('l')
-// //       // Set the color of the embed
-// //       .setColor(0xff0000)
-// //       // Set the main content of the embed
-// //       .setDescription('Hello, this is a \n slick embed!');
+    if(indOfDot<=7)
+    title = "Something goes wrong";
+    else
+    title = msg.content.slice(7,indOfDot);
 
-// //   // client.channels.fetch('784545317432983573').send('hi');
 
-// //   }
+ while(indOfDot!=-1)
+ {
+  content+=msg.content.slice(indOfDot+1,msg.content.indexOf('.', indOfDot+1));
+  content+='\n';
+ indOfDot = msg.content.indexOf('.', indOfDot+1);
+ console.log("contnent"+msg.content.length);
+ console.log(indOfDot);
+
+
+
+ }
+
+
+        const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle(title)
+      // Set the color of the embed
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription(content);
+
+  client.channels.fetch('784545317432983573')
+     .then(channel => channel.send(embed));
+  }
   
-// })
+})
 
 
 //
